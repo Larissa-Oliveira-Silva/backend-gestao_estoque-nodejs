b4b91642918b53cfc83fc0621f86e658932f9d49
Larissa-Oliveira-Silva <larissa-.oliveira@hotmail.com>
2021-10-28 21:33:36 -0300

    Construindo um servidor com express

---
+++ server.js
@@ -0,0 +1,20 @@
+'use-strict'
+
+const express = require('express');
+const app = express();
+
+const http = require('http').Server(app);
+
+http.listen(3000, () => {
+  console.info("Api do sistemas de gestão de estoque inicializada com sucesso!");
+})
+
+app.use('/bem-vindo/:nome', (req,res) =>{
+
+  res.send("Bem vindo ao sistema de gestão de estoque "+req.params.nome);
+})
+
+app.use('/lista-produtos:id', (req,res) =>{
+
+  res.send("Listando o produto que possui o ID "+req.params.id);
+})
