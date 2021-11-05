d8641693640e20583255cb8ea811857c47fdb016
Larissa-Oliveira-Silva <larissa-.oliveira@hotmail.com>
2021-11-04 21:08:52 -0300

    Criação dos endpoints listar produtos

---
+++ mysql.js
@@ -0,0 +1,13 @@
+const mysql = require ('mysql')
+
+var pool = mysql.createPool({
+
+  host:"localhost",
+  port:"3306",
+  user:"root",
+  password:"root",
+  database:"estoque"
+})
+
+//Necessario para conseguir acessar o pool fora do arquivo
+exports.pool = pool;
