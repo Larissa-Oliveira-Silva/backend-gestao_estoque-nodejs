993f832e0d21a14c6fe8d55075b643898c27ff1a
Larissa-Oliveira-Silva <larissa-.oliveira@hotmail.com>
2021-12-02 20:40:39 -0300

    Implantação de segurança a api

--- server.js
+++ server.js
@@ -2,8 +2,9 @@
 
 const app = require('./bin/express');
 const http = require('http').Server(app);
+const variables = require('./bin/configuration/variables');
 
-http.listen(3000, () => {
-  console.info("Api do sistemas de gestão de estoque inicializada com sucesso!");
+http.listen(variables.Api.port, () => {
+  console.info("Api do sistemas de gestão de estoque inicializada com sucesso!", variables.Api.port);
 })
 
